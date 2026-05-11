698e4ef31a55f94ca78facb65c9f0029afba4e54
Mostafa Safwat <mostafa.safwat@contellect.com>
2026-05-11 09:28:07 +0300

    refactor: Update getEnv.sh to use git commands for app name and version retrieval

--- getEnv.sh
+++ getEnv.sh
@@ -1,14 +1,26 @@
 dir=$(dirname "$0")
-APP_NAME=$(npm pkg get name | jq -r)
-APP_VERSION=$(npm pkg get version | jq -r)
-TAG=$APP_NAME:v$APP_VERSION-dev
-ENTRYPOINT=$(npm pkg get bin | jq -r)
-
+APP_NAME=$(git remote get-url origin | sed 's/.*\///' | sed 's/\.git//')
+APP_VERSION=$(git describe --tags --dirty=-d --always)
+TAG="$APP_NAME:$APP_VERSION"
+APP_TYPE="unknown" #node|dotnet|angular
+if false ; then
+  echo "No application type detected."
+elif [ -f "*.csproj" ]; then
+  APP_TYPE="dotnet"
+elif [ -f "angular.json" ]; then
+  APP_TYPE="angular"
+elif [ -f "package.json" ]; then
+  APP_TYPE="node"
+fi
+export dir
 export TAG
 export APP_NAME
 export APP_VERSION
-export ENTRYPOINT
-echo "ENTRYPOINT=$ENTRYPOINT"
-echo "APP_NAME=$APP_NAME"
-echo "APP_VERSION=$APP_VERSION"
-echo "TAG=$TAG"
+export APP_TYPE
+echo "=============================================================="
+echo "  dir : $dir"
+echo "  APP_NAME : $APP_NAME"
+echo "  APP_VERSION : $APP_VERSION"
+echo "  TAG : $TAG"
+echo "  APP_TYPE : $APP_TYPE"
+echo "=============================================================="
