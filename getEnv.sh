8d966b6962c80f192ba34b257251088b3ef1ae7b
Mostafa Safwat <mostafa.safwat@contellect.com>
2026-05-13 12:20:07 +0300

    feat: Enhance build process with asset copying and environment setup

--- getEnv.sh
+++ getEnv.sh
@@ -1,7 +1,9 @@
 dir=$(dirname "$0")
 APP_NAME=$(git remote get-url origin | sed 's/.*\///' | sed 's/\.git//')
 APP_VERSION=$(git describe --tags --dirty=-d --always)
-TAG="$APP_NAME:$APP_VERSION"
+REPOSITORY=dev.local:5005
+TAG="$REPOSITORY/$APP_NAME:$APP_VERSION"
+LatestTag="$REPOSITORY/$APP_NAME:latest"
 APP_TYPE="unknown" #node|dotnet|angular
 if false ; then
   echo "No application type detected."
@@ -17,10 +19,13 @@ export TAG
 export APP_NAME
 export APP_VERSION
 export APP_TYPE
+export REPOSITORY
 echo "=============================================================="
 echo "  dir : $dir"
 echo "  APP_NAME : $APP_NAME"
 echo "  APP_VERSION : $APP_VERSION"
 echo "  TAG : $TAG"
+echo "  LatestTag : $LatestTag"
 echo "  APP_TYPE : $APP_TYPE"
+echo "  REPOSITORY : $REPOSITORY"
 echo "=============================================================="
